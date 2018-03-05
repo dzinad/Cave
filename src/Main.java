5ba011548d8ecd65aa5dea7fd25535a7b9b460d4
Dzina Dybouskaya <miss.dybowskaya@gmail.com>
2018-03-06 02:40:51 +0300

    algorithm by diz norm form

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,6 @@ class InnerWay {
 		complexity = 0;
         int prev = start;
         for (Integer w: innerGrots) {
-        	//coverage[key1][w] = coverage[key2][w] = true;
         	complexity += Main.matrix[prev][w];
         	prev = w;
         }
@@ -53,27 +52,24 @@ public class Main {
     private static final String INPUT_FILE_NAME = "input.in";
     private static final String OUTPUT_FILE_NAME = "output.out";
     private static int n, k, junctionsNum;
-    private static Stack<Integer> way;
-    private static Stack<Integer> innerWay = new Stack<>();
     public static int[][] matrix;
     private static int[][] matrixCopy;
     private static int[] starts, ends;
-    //private static HashMap<Integer, InnerWay> innerWays;
     private static InnerWay[] innerWays;
     private static int innerWaysLength = 0;
-    private static int outerRingComplexity = 0;
-
+    private static List<Integer> finalWay = new ArrayList<>();
+    private static HashSet<InnerWay> finalInnerWay;
+    
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
+        //long start = System.currentTimeMillis();
         initData();
         processData();
-        //outputData();
-        long end = System.currentTimeMillis();
-        System.out.println("time: " + (end - start));
+        outputData();
+        //long end = System.currentTimeMillis();
+        //System.out.println("time: " + (end - start));
     }
 
     private static void initData() {
-        way = new Stack<>();        
         Scanner scanner = null;
         try {
             scanner = new Scanner(new File(INPUT_FILE_NAME));
@@ -90,7 +86,6 @@ public class Main {
         }
         junctionsNum = 3 * n / 2;
         int start, end, hard;
-        //innerWays = new HashMap<>();
         innerWays = new InnerWay[k];
         starts = new int[junctionsNum + 1];
         ends = new int[junctionsNum + 1];
@@ -101,27 +96,16 @@ public class Main {
             starts[i] = start;
             ends[i] = end;
             matrix[start][end] = matrix[end][start] = matrixCopy[start][end] = matrixCopy[end][start] = hard;
-            if (start <= k && end <= k) {
-            	outerRingComplexity += hard;
-            }
         }
     }
 
     private static void outputData() {
-		/*System.out.println("way:");
-		if (way == null) {
-			System.out.println("way null");
-			return;
-		}*/
-        way.pop();
-        int last = way.pop();
+        int last = finalWay.remove(finalWay.size() - 1);
         StringBuffer sb = new StringBuffer();
-        for (Integer w: way) {
+        for (Integer w: finalWay) {
             sb.append(w + " ");
-            //System.out.print(w + " ");
         }
         sb.append(last + "");
-        //System.out.println("");
         BufferedWriter writer;
         try {
             writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_NAME));
@@ -137,33 +121,61 @@ public class Main {
             if (starts[i] <= k && ends[i] <= k && starts[i] != ends[i]) {
                 Stack<Integer> innerWay = findInnerWayFromOuterToOuter(starts[i], ends[i]); 
                 InnerWay tmp = new InnerWay(starts[i], ends[i], innerWay);
-                //int key1 = starts[i] * k + ends[i];
-                //int key2 = ends[i] * k + starts[i];
                 innerWays[innerWaysLength] = tmp;
                 innerWaysLength++;
-                //innerWays.put(key1, tmp);
-                //innerWays.put(key2, tmp);
             }
         }
         
         HashSet<HashSet<InnerWay>> coverage = findCoverages(k + 1);
-        System.out.println("coverages: ");
+        Integer minComplexity = null;
         for (HashSet<InnerWay> conj: coverage) {
-        	for (InnerWay iw: conj) System.out.print(iw.getStart() + "-" + iw.getEnd() + " and ");
-        	System.out.println("\nor");
+        	int curComplexity = 0;
+        	for (InnerWay iw: conj) {
+        		curComplexity += iw.getComplexity() - matrix[iw.getStart()][iw.getEnd()];
+        	}
+        	if (minComplexity == null || curComplexity < minComplexity) {
+        		minComplexity = curComplexity;
+        		finalInnerWay = conj;
+        	}
         }
-        
-        /*boolean[] used = new boolean[n + 1];
-        List<InnerWay> usedInnerWays = findCoverageSets(0, used);
-        System.out.println("used inner ways: ");
-        for (InnerWay iw: usedInnerWays) {
-        	System.out.println(iw.getStart() + " - " + iw.getEnd());
-        }*/
-        /*for (InnerWay iw: innerWays) {
-        	System.out.println(iw.getInnerGrots());
-        }*/
+
+        findFinalWay(1, 1);
     }
 
+    private static void findFinalWay(int from, int grot) {
+    	boolean innerWayAdded = false;
+    	for (InnerWay iw: finalInnerWay) {
+    		if (iw.getStart() == from && iw.getEnd() == grot) {
+    			finalWay.addAll(iw.getInnerGrots());
+    			finalWay.add(grot);
+    			innerWayAdded = true;
+    			break;
+    		}
+    		if (iw.getEnd() == from && iw.getStart() == grot) {
+    			Stack<Integer> tmp = iw.getInnerGrots();
+    			while(!tmp.empty()) {
+    				finalWay.add(tmp.pop());
+    			}
+    			finalWay.add(grot);
+    			innerWayAdded = true;
+    			break;
+    		}
+    	}
+    	if (grot == 1 && from != 1) {
+    		return;
+    	}
+    	for (int i = 1; i <= k; i++) {
+    		if (matrix[grot][i] >= 0 && i != from) {
+    			if (!innerWayAdded) {
+    				finalWay.add(grot);
+    			}
+    			findFinalWay(grot, i);
+    			break;
+    		}
+    		
+    	}
+    }
+    
 
     private static HashSet<HashSet<InnerWay>> findCoverages(int startGrot) {
     	HashSet<HashSet<InnerWay>> res = new HashSet<>();
@@ -184,7 +196,6 @@ public class Main {
     			for (HashSet<InnerWay> conj: tmp) {
     				HashSet<InnerWay> newConj = new HashSet<>();
     				newConj.addAll(conj);
-    				Stack<Integer> coveredGrots = new Stack<>();
     				boolean shouldAdd = true;
     				for (InnerWay oneWay: conj) {
     					if (oneWay.equals(iw)) {
@@ -220,27 +231,6 @@ public class Main {
         			for (HashSet<InnerWay> tr: toRemove) {
         				res.remove(tr);
         			}
-    				/*Stack<Integer> coveredGrots = new Stack<>();
-        			for (HashSet<InnerWay> prevConj: res) {
-        				for (InnerWay oneWay: prevConj) {
-            				coveredGrots.addAll(oneWay.getInnerGrots());            				
-        				}
-        			}
-        			
-        			for (InnerWay oneWay: newConj) {
-        				for (Integer grot: oneWay.getInnerGrots()) {
-        					if (coveredGrots.contains(grot)) {
-        						shouldAdd = false;
-        						break;
-        					}
-        				}
-        				if (!shouldAdd) {
-        					break;
-        				}
-        			}
-        			if (!shouldAdd) {
-        				continue;
-        			}*/
         			res.add(newConj);        				
     			}
     		}
@@ -248,78 +238,6 @@ public class Main {
     	return res;
     }
     
-    private static List<InnerWay> findCoverageSets(int startIndex, boolean[] used) {
-    	InnerWay curWay = innerWays[startIndex];
-    	System.out.print("curway: " + curWay.getInnerGrots() + ", used: ");
-    	for (boolean b: used) System.out.print(b + " ");
-    	System.out.println("");
-    	
-    	List<InnerWay> ways = new ArrayList<>();
-    	Stack<Integer> coverage = curWay.getInnerGrots();
-    	ways.add(curWay);
-    	if (startIndex == k - 1) {
-    		boolean ifCovered = true;
-    		for (int i = k + 1; i <= n; i++) {
-    			if (used[i] == false) {
-    				ifCovered = false;
-    				break;
-    			}
-    		}
-        	for (Integer grot: coverage) {
-        		used[grot] = false;
-        	}
-    		if (!ifCovered) {
-    			return null;
-    		}
-        	return ways;    		
-    	}
-
-    	boolean isWayOK = true;
-    	int index = -1;
-    	for (Integer grot: coverage) {
-    		if (used[grot]) {
-    			index = grot;
-    			isWayOK = false;
-    			break;
-    		}
-    		used[grot] = true;
-    	}
-    	if (!isWayOK) {
-        	for (Integer grot: coverage) {
-        		if (grot == index) {
-        			used[grot] = true;
-        			break;
-        		}
-        		used[grot] = false;
-        	}
-    		return null;
-    	}
-    	
-    	int minComplexity = -1;
-    	for (int i = startIndex + 1; i < k; i++) {
-    		int complexity = outerRingComplexity + curWay.getComplexity() - matrix[curWay.getStart()][curWay.getEnd()];
-        	List<InnerWay> nextWays = findCoverageSets(i, used);
-        	if (nextWays == null) {        		
-        		continue;
-        	}
-        	for (InnerWay iw: nextWays) {
-        		complexity += (iw.getComplexity() - matrix[iw.getStart()][iw.getEnd()]);
-        	}
-        	if (complexity < minComplexity || minComplexity == -1) {
-        		minComplexity = complexity;
-        		ways.clear();
-        		ways.add(curWay);
-        		ways.addAll(nextWays);
-        	}
-    	}
-    	for (Integer grot: coverage) {
-    		used[grot] = false;
-    	}
-    	if (minComplexity == -1) {
-    		return null;
-    	}
-    	return ways;
-    }
     private static Stack<Integer> findInnerWayFromOuterToOuter(int start, int end) {
         if (start == end) {
             return new Stack<Integer>();
